ac9c684dc058d471ac6c8e58e7ff1f0605fbea24
yashaswi <yashaswi2000@gmail.com>
2020-09-12 15:52:01 +0530

    commit 1

---
+++ personal-web/src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/Header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText='Yashaswi'/>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
---
+++ personal-web/src/pages/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Home() {
+  return <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+}
